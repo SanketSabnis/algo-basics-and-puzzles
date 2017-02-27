637a5cd4b1a0ac5f0bb0dcf91b649231345ccc50
Riley Gibbs <riley@citizennet.com>
2017-02-27 14:38:14 -0800

    Added Quicksort implementation

---
+++ Grokking/swift/Quicksort.playground/Contents.swift
@@ -0,0 +1,18 @@
+func quicksort(_ coll: Array<Int>) -> Array<Int> {
+    if (coll.count < 2) {
+        return coll;
+    }
+    let pivotSpot = coll.count / 2;
+    let pivot = coll[pivotSpot];
+    var left = Array<Int>();
+    var right = Array<Int>();
+    for index in 0...coll.count-1 {
+        if (coll[index] < pivot) {
+            left.append(coll[index]);
+        } else if (index != pivotSpot) {
+            right.append(coll[index]);
+        }
+    }
+    let pivotArray = [pivot];
+    return quicksort(left) + pivotArray + quicksort(right);
+}
