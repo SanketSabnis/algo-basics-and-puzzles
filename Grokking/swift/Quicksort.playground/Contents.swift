7b97ae134136d1cdec32245e1d5d761226d6cf8a
Riley Gibbs <riley@citizennet.com>
2017-02-27 18:19:02 -0800

    Got it working with a pivot in the middle of the array. ArraySlices are tricky, man\!

--- Grokking/swift/Quicksort.playground/Contents.swift
+++ Grokking/swift/Quicksort.playground/Contents.swift
@@ -1,18 +1,16 @@
 func quicksort<T: Comparable>(_ coll: Array<T>) -> Array<T> {
-    if (coll.count < 2) {
-        return coll;
-    }
+    // Base case
+    if (coll.count < 2) { return coll }
+    // Recursive case
     let pivotSpot = coll.count / 2;
     let pivot = coll[pivotSpot];
-    var left = Array<T>();
-    var right = Array<T>();
-    for index in 0...coll.count-1 {
-        if (coll[index] < pivot) {
-            left.append(coll[index]);
-        } else if (index != pivotSpot) {
-            right.append(coll[index]);
-        }
-    }
+    let trimmedColl = coll.prefix(upTo: pivotSpot)
+        + coll.suffix(from: pivotSpot+1);
+    let left = trimmedColl.filter({$0 < pivot});
+    let right = trimmedColl.filter({$0 >= pivot});
     let pivotArray = [pivot];
     return quicksort(left) + pivotArray + quicksort(right);
 }
+
+var myArray = [1,2,3,4,3,2,4,3,5,4,5,6];
+quicksort(myArray);
