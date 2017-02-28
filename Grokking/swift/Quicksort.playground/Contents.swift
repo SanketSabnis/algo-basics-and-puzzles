66ac8fb5fb2cf3e0f4f25e0bf4e0d7d649cd206e
Riley Gibbs <riley@citizennet.com>
2017-02-27 17:47:03 -0800

    Used generics in the function definition. Thanks, Chris\!

--- Grokking/swift/Quicksort.playground/Contents.swift
+++ Grokking/swift/Quicksort.playground/Contents.swift
@@ -1,11 +1,11 @@
-func quicksort(_ coll: Array<Int>) -> Array<Int> {
+func quicksort<T: Comparable>(_ coll: Array<T>) -> Array<T> {
     if (coll.count < 2) {
         return coll;
     }
     let pivotSpot = coll.count / 2;
     let pivot = coll[pivotSpot];
-    var left = Array<Int>();
-    var right = Array<Int>();
+    var left = Array<T>();
+    var right = Array<T>();
     for index in 0...coll.count-1 {
         if (coll[index] < pivot) {
             left.append(coll[index]);
