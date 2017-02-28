c71833870caacab15b665707a13e3af7c2e19617
Aaron Chu <ac3270@gmail.com>
2017-02-28 11:45:59 -0800

    R quicksort

---
+++ Grokking/R/quicksort.R
@@ -0,0 +1,10 @@
+quicksort <- function(vec) {
+	if (length(vec) < 1) return(vec)
+	pivot <- vec[1]
+	left <- vec[vec < pivot]
+	right <- vec[vec > pivot]
+	c(quicksort(left), vec[vec == pivot], quicksort(right))
+}
+
+print(quicksort(c(8, 2, 6, 5, 1, 3, 7)))
+print(quicksort(c("Zebra", "Bird", "Panda", "Dog", "Cat")))
